b25654150022bcc37ec4f1b213f17df4c795ad9c
kendavis416 <ken.davis@d2solutionsgroup.com>
2016-05-18 00:00:25 -0400

    Latest and greatest ... but still have work to do

--- js/app.js
+++ js/app.js
@@ -21,40 +21,87 @@ $('#selectSource').on('click', 'li', function(event) {
 
 });
 
+function createArticle(title,categories,image1,scores) {
+	return('<article class="article">' +
+           '<section class="featureImage">' +
+              '<img src=' + image1 + ' alt="" />' +
+            '</section>' + 
+            '<section class="articleContent">' +
+                '<a href="#"><h3 id="articleTitle">' + title + '</h3></a>' +
+                '<h6 id="feedCategory">' + categories + '</h6>' +
+            '</section>' +
+            '<section class="impressions">' + scores + '</section>' +
+            '<div class="clearfix"></div>' +
+          '</article>');
+}
+
 function displayFeed(myCurrentFeed) {
 	//console.log(myCurrentFeed);
-	case "Reddit": {
-		//Do something here;
-		$.ajax{
-			url:'',
-			datatype: 'json',
-			data:{},
-			method: 'Get',
-			success: function(response) {
-				var myRedditObj = response.data.children;
-				myRedditObj.forEach(function(item) {
-					console.log(item.data.author);
-				});
-			},
-			error: function(response) {
-				console.log('There was a problem');
-			}
-		}
-		console.log("I Reddit");
-		break;
-	}
-	case "Mashable": {
-		//Do something here;
-		console.log("Mashin' some taters!");
-		break;
-	}
-	case "Digg": {
-		//DoSomethingHere;
-		console.log("Do you Digg it!");
-		break;
-	}
-	default {
-		//do your default action here;
-		console.log("This is your default action.");
+	switch (myCurrentFeed) {
+		case "Reddit": 
+			//Do something here;
+			$.ajax({
+				url:'https://www.reddit.com/top.json',
+				datatype: 'json',
+				data:{},
+				method: 'Get',
+				success: function(response) {
+					var allarticles = "";
+					var myRedditObj = response.data.children;
+					myRedditObj.forEach(function(item) {
+						if (item.data.preview !== undefined) {
+							//console.log(item);
+							var title = item.data.title;
+							var featureImg = item.data.preview.images[0].source.url;
+							var categories = item.data.subreddit;
+							var scores = item.data.score;
+							allarticles+=this.createArticle(title,categories,featureImg,scores);
+						}
+					});
+					$('#main').html(allarticles);
+				},
+				error: function(response) {
+					console.log('There was a problem with the Reddit feed');
+				}
+			});
+			break;
+		case "Mashable": 
+			//Do something here;
+			$.ajax({
+				url:'http://www.mashable.com/stories.json',
+				datatype: 'json',
+				data: {},
+				method: 'Get',
+				success: function(response) {
+					console.log(response);
+					var allarticles = "";
+					var myMashableObj = response.new;
+					myMashableObj.forEach(function(item) {
+						console.log(item);
+						if (item.feature_image !== undefined) {
+							var title = item.title;
+							var featureImg = item.feature_image;
+							var categories = item.channel_label;
+							var scores = item.shares.total;
+							allarticles+=this.createArticle(title,categories,featureImg,scores);							
+						}
+					});
+					$('#main').html(allarticles);
+					console.log("MASHUP:>>>> ",response);
+				},
+				error: function(response) {
+					console.log('There was a problem mashin them taters!');
+				}
+			});
+			console.log("We are mashin some taters!");
+			break;
+		case "Digg": 
+			//DoSomethingHere;
+			console.log("Do you Digg it!");
+			break;
+		default: 				
+			//do your default action here;
+			console.log("This is your default action.");
+				
 	}
 }
